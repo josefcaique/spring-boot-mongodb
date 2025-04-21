2a24884f5819fe15c6e674bf5bf13b42f81ef733
Josef Caique <jcaique10@gmail.com>
2025-04-21 18:52:12 -0300

    Creating entity Post

---
+++ src/main/java/com/josef/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.josef.workshopmongo.repository;
+
+import com.josef.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
