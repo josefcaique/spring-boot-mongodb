11fd4432d3516a3f1899daf859f4149fb81d52c4
Josef Caique <jcaique10@gmail.com>
2025-04-21 23:42:14 -0300

    simple query with query methods

--- src/main/java/com/josef/workshopmongo/resources/PostResource.java
+++ src/main/java/com/josef/workshopmongo/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.josef.workshopmongo.resources;
 
 import com.josef.workshopmongo.domain.Post;
+import com.josef.workshopmongo.resources.util.URL;
 import com.josef.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -17,6 +20,14 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = service.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+
+       List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
--- src/main/java/com/josef/workshopmongo/services/PostService.java
+++ src/main/java/com/josef/workshopmongo/services/PostService.java
@@ -20,4 +20,7 @@ public class PostService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
+    }
 }
