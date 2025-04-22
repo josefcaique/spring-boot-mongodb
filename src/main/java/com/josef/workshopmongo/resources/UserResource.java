6d4f2995d39591da3fdfee544cbc410675f30aa8
Josef Caique <jcaique10@gmail.com>
2025-04-21 21:24:40 -0300

    creating endpoint to return users posts

--- src/main/java/com/josef/workshopmongo/resources/UserResource.java
+++ src/main/java/com/josef/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.josef.workshopmongo.resources;
 
+import com.josef.workshopmongo.domain.Post;
 import com.josef.workshopmongo.domain.User;
 import com.josef.workshopmongo.dto.UserDTO;
 import com.josef.workshopmongo.services.UserService;
@@ -55,5 +56,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user.getPosts());
     }
 }
